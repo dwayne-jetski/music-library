9d7086a584818639d7fdb45da8aa1f599c05a9bb
marcus-baumgardner <dwayne.jerski@gmail.comgit config --global user.name marcus-baumgardnergit config --global user.email dwayne.jerski@gmail.com>
2020-11-17 20:32:45 -0600

    Created funciton to build out table, added styling to table in .js file to be imported in index.html

---
+++ music-library.js
@@ -0,0 +1,27 @@
+"use strict"
+
+$.ajax({
+    url: "http://www.devcodecampmusiclibrary.com/api/music",
+    dataType: "json",
+    type: "get",
+    success: function(response){
+        buildTable(response);
+    }
+});
+
+
+function buildTable(data){
+    var table = $('#my-table');
+    var row
+    for(var i = 0; i < data.length; i++){
+        $(`#my-table`).append(
+        `<tr>
+            <td class= "table-border" >${data[i].title}</td>
+            <td class= "table-border">${data[i].album}</td>
+            <td class= "table-border">${data[i].artist}</td>
+            <td class= "table-border">${data[i].genre}</td>
+            <td class= "table-border">${data[i].releaseDate}</td>
+        </tr>`
+        );       
+    }  
+}
