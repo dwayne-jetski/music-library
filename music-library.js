49099f676ce8ff4507dc758180e85c0335a4c978
marcus-baumgardner <dwayne.jerski@gmail.comgit config --global user.name marcus-baumgardnergit config --global user.email dwayne.jerski@gmail.com>
2020-11-18 21:19:31 -0600

    final build. Added some styling to the table to make it look pretty.

--- music-library.js
+++ music-library.js
@@ -10,17 +10,28 @@ $.ajax({
 });
 
 function buildTable(data){
-    var table = $('#my-table');
-    var row
     for(var i = 0; i < data.length; i++){
-        $(`#my-table`).append(
-        `<tr>
-            <td class= "table-border" >${data[i].title}</td>
-            <td class= "table-border">${data[i].album}</td>
-            <td class= "table-border">${data[i].artist}</td>
-            <td class= "table-border">${data[i].genre}</td>
-            <td class= "table-border">${data[i].releaseDate}</td>
-        </tr>`
-        );       
+
+         if(i%2==0){     
+            $(`#my-table`).append(
+            `<tr>
+                <td class= "table-border even-row-styles">${data[i].title}</td>
+                <td class= "table-border even-row-styles">${data[i].album}</td>
+                <td class= "table-border even-row-styles">${data[i].artist}</td>
+                <td class= "table-border even-row-styles">${data[i].genre}</td>
+                <td class= "table-border even-row-styles">${data[i].releaseDate}</td>
+            </tr>`
+            );
+         }
+        else{
+            $(`#my-table`).append(
+                `<tr>
+                    <td class= "table-border odd-row-styles">${data[i].title}</td>
+                    <td class= "table-border odd-row-styles">${data[i].album}</td>
+                    <td class= "table-border odd-row-styles">${data[i].artist}</td>
+                    <td class= "table-border odd-row-styles">${data[i].genre}</td>
+                    <td class= "table-border odd-row-styles">${data[i].releaseDate}</td>
+                </tr>`);
+        }       
     }  
 }
